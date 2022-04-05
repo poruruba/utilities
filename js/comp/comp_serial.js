ed0a024881a4db7600107ef4992112ad600c0d93
poruruba <44470781+poruruba@users.noreply.github.com>
2022-04-06 00:39:10 +0900

    Update comp_serial.js

--- js/comp/comp_serial.js
+++ js/comp/comp_serial.js
@@ -5,20 +5,22 @@ export default {
   template: `
 <div>
   <h2 class="modal-header">シリアル</h2>
-  <button class="btn btn-secondary" v-on:click="connect(!connected)"><span v-if="!connected">Connect</span><span v-else>Disconnect</span></button>
-  <label class="title"> baudrate: </label>
-  <span class="col-auto">
-    <select v-if="!connected" class="form-select" v-model.number="baud">
-      <option value="9600">9600</option>
-      <option value="57600">57600</option>
-      <option value="115200">115200</option>
-      <option value="230400">230400</option>
-      <option value="921600">921600</option>
-    </select>
-  </span>
-  <label class="title"> connected:</label> {{connected}}
-  <button class="btn btn-secondary btn-sm float-end" v-on:click="text_clear">clear</button>
-  <span class="float-end"><input type="checkbox" v-model="auto_scroll" id="auto_scroll"><label for="auto_scroll"> auto-scroll </label></span>
+  <div class="row">
+    <button class="btn btn-secondary" v-on:click="connect(!connected)"><span v-if="!connected">Connect</span><span v-else>Disconnect</span></button>
+    <label class="title"> baudrate: </label>
+    <span class="col-auto">
+      <select v-if="!connected" class="form-select" v-model.number="baud">
+        <option value="9600">9600</option>
+        <option value="57600">57600</option>
+        <option value="115200">115200</option>
+        <option value="230400">230400</option>
+        <option value="921600">921600</option>
+      </select>
+    </span>
+    <label class="title"> connected:</label> {{connected}}
+    <button class="btn btn-secondary btn-sm float-end" v-on:click="text_clear">clear</button>
+    <span class="float-end"><input type="checkbox" v-model="auto_scroll" id="auto_scroll"><label for="auto_scroll"> auto-scroll </label></span>
+  </div>
   <textarea class="form-control" style="height: 70vh" id="el" readonly>{{console_text}}</textarea>
 </div>`,
   data: function () {
