ab3557b81003b0003efc1a4da3d889775b3ee22b
poruruba <44470781+poruruba@users.noreply.github.com>
2024-01-05 12:56:32 +0900

    Update start.js

--- js/start.js
+++ js/start.js
@@ -33,6 +33,7 @@ const TAB_LIST = [
 	{ id: 'oidc', name: 'OIDC' },
 	{ id: 'serial', name: 'シリアル' },
 	{ id: 'map', name: 'マップ' },
+	{ id: 'geocode', name: 'Geocoding' },
 	{ id: 'help', name: 'ヘルプ' },
 ];
 
@@ -129,6 +130,7 @@ import comp_regex from './comp/comp_regex.js';
 import comp_oidc from './comp/comp_oidc.js';
 import comp_serial from './comp/comp_serial.js';
 import comp_map from './comp/comp_map.js';
+import comp_geocode from './comp/comp_geocode.js';
 vue_add_component(vue_options, "comp_arrange", comp_arrange);
 vue_add_component(vue_options, "comp_blecent", comp_blecent);
 vue_add_component(vue_options, "comp_uuid", comp_uuid);
@@ -157,5 +159,6 @@ vue_add_component(vue_options, "comp_regex", comp_regex);
 vue_add_component(vue_options, "comp_oidc", comp_oidc);
 vue_add_component(vue_options, "comp_serial", comp_serial);
 vue_add_component(vue_options, "comp_map", comp_map);
+vue_add_component(vue_options, "comp_geocode", comp_geocode);
 
 window.vue = new Vue( vue_options );
