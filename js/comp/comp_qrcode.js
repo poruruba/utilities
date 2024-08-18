f5abb60d9212a3eb51dafd76dd4b1a7e46201f60
poruruba <44470781+poruruba@users.noreply.github.com>
2024-08-18 13:32:09 +0900

    Add files via upload

--- js/comp/comp_qrcode.js
+++ js/comp/comp_qrcode.js
@@ -20,7 +20,7 @@ export default {
       </span>
   </collapse-panel>
 
-  <collapse-panel id="qrcode_scan_panel" title="QRコードスキャン" collapse="true">
+  <collapse-panel id="qrcode_scan_panel" title="QRコードスキャン(カメラ)" collapse="true">
       <span slot="content">
           <div class="card-body">
               <label class="title">scaned data</label>
@@ -28,9 +28,9 @@ export default {
                   <button class="btn btn-secondary oi oi-paperclip" v-on:click="clip_copy(qrcode_scaned_data)"></button>
                   <input type="text" class="form-control" v-model="qrcode_scaned_data" readonly>
               </div><br>
-              <button class="btn btn-primary" v-on:click="qrcode_scan()">{{qrcode_btn}}</button><br>
+              <button class="btn btn-primary" v-on:click="qrcode_scan">{{qrcode_btn}}</button><br>
               <div>
-                  <img class="img-fluid" v-show="!qrcode_running && qrcode_scaned_data==''" id="qrcode_start" src="./img/qrcode_start.png"><br>
+                  <img class="img-fluid" v-show="!qrcode_running && qrcode_scaned_data==''" id="qrcode_start" src="./img/qrcode_start.png">
                   <video class="img-fluid" v-show="qrcode_running" id="qrcode_camera" autoplay playsinline></video>
                   <canvas class="img-fluid" v-show="!qrcode_running && qrcode_scaned_data!=''" id="qrcode_canvas"></canvas>
               </div>
@@ -40,21 +40,145 @@ export default {
           </div>
       </span>
   </collapse-panel>
+
+    <collapse-panel id="qrcode_file_panel" title="QRコードスキャン(画像)" collapse="true">
+      <span slot="content">
+          <div class="card-body">
+              <label class="title">scaned data2</label>
+              <div class="input-group">
+                  <button class="btn btn-secondary oi oi-paperclip" v-on:click="clip_copy(qrcode_scaned_data2)"></button>
+                  <input type="text" class="form-control" v-model="qrcode_scaned_data2" readonly>
+              </div><br>
+              <comp_file id="image_file" v-bind:callback="image_open_files" ref="image_file"></comp_file><br>
+              <div class="row">
+                <div class="col-4">
+                  <textarea placeholder="ここに画像かテキストをペースト(Ctrl-V)してください。" class="form-control" style="text-align: center; resize: none;" rows="5" 
+                    v-on:paste="do_paste" v-on:drop.prevent="do_file_paste" v-on:dragover.prevent readonly>
+                  </textarea>
+                  <br>
+                </div>
+                <div class="col-8">
+                  <img class="img-fluid" v-show="qrcode_scaned_data2==''" id="qrcode_start2" src="./img/qrcode_start.png">
+                  <canvas class="img-fluid" v-show="qrcode_scaned_data2!=''" id="qrcode_canvas2"></canvas>
+                </div>
+              </div>
+          </div>
+          <div class="card-footer">
+              <button class="btn btn-secondary" data-bs-toggle="collapse" href="#qrcode_file_panel">閉じる</button>
+          </div>
+      </span>
+  </collapse-panel>
+
 </div>`,
   data: function () {
     return {
       qrcode_input: '',
       qrcode_btn: 'QRスキャン開始',
       qrcode_video: null,
-      qrcode_canvas: null,
       qrcode_context: null,
       qrcode_scaned_data: '',
+      qrcode_scaned_data2: '',
+
       qrcode_running: false,
       qrcode_timer: null,
     }
   },
   methods: {
     /* QRコード */
+    image_open_files: function (files) {
+      if( files.length == 0 ){
+        return;
+      }
+      var file = files[0];
+      if (!file.type.startsWith('image/')) {
+        alert('画像ファイルではありません。');
+        return;
+      }
+
+      var reader = new FileReader();
+      reader.onload = (e) => {
+          var data_url = e.target.result;
+          this.set_dataurl(data_url);
+      };
+      reader.readAsDataURL(file);
+    },
+    do_file_paste: function(e){
+        console.log(e);
+        if( e.dataTransfer.files.length == 0 )
+            return;
+
+        var file = e.dataTransfer.files[0];
+        console.log(file.type);
+
+        if(file.type.startsWith('image/')){
+            var reader = new FileReader();
+            reader.onload = (e) => {
+                var data_url = e.target.result;
+                this.set_dataurl(data_url);
+            };
+            reader.readAsDataURL(file);
+        }else{
+            alert('サポートしていません。');
+        }
+    },
+    do_paste: async function(e){
+        console.log(e);
+        if (e.clipboardData.types.length == 0)
+            return;
+
+        for( let item of e.clipboardData.items){
+          if( item.type.startsWith('image/')){
+              var imageFile = item.getAsFile();
+              var reader = new FileReader();
+              reader.onload = (e) => {
+                  var data_url = e.target.result;
+                  this.set_dataurl(data_url);
+              };
+              reader.readAsDataURL(imageFile);
+              return;
+          }
+        }
+        alert('サポートしていません。');
+    },
+    set_dataurl: function(data_url){
+      this.qrcode_scaned_data2 = "";
+      var image = document.querySelector('#qrcode_start2');
+      image.onload = () =>{
+        var qrcode_canvas = document.querySelector('#qrcode_canvas2');
+        qrcode_canvas.width = image.naturalWidth;
+        qrcode_canvas.height = image.naturalHeight;
+        var ctx = qrcode_canvas.getContext('2d');
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(image, 0, 0);
+        const imageData = ctx.getImageData(0, 0, image.naturalWidth, image.naturalHeight);
+
+        const code = jsQR(imageData.data, image.naturalWidth, image.naturalHeight);
+        if (code && code.data != "") {
+          this.qrcode_scaned_data2 = code.data;
+          console.log(code);
+  
+          ctx.strokeStyle = "blue";
+          ctx.lineWidth = 3;
+  
+          var pos = code.location;
+          ctx.beginPath();
+          ctx.moveTo(pos.topLeftCorner.x, pos.topLeftCorner.y);
+          ctx.lineTo(pos.topRightCorner.x, pos.topRightCorner.y);
+          ctx.lineTo(pos.bottomRightCorner.x, pos.bottomRightCorner.y);
+          ctx.lineTo(pos.bottomLeftCorner.x, pos.bottomLeftCorner.y);
+          ctx.lineTo(pos.topLeftCorner.x, pos.topLeftCorner.y);
+          ctx.stroke();
+        } else {
+          this.toast_show("QRコードは見つけられませんでした。")
+        }
+      };
+      image.onerror = (e) =>{
+        console.error(e);
+        alert(e);
+      };
+      image.src = data_url;
+    },
+
     qrcode_create: function () {
       var element = document.querySelector('#qrcode_area');
       element.innerHTML = '';
@@ -62,7 +186,6 @@ export default {
     },
     qrcode_scan: function () {
       this.qrcode_video = document.querySelector('#qrcode_camera');
-      this.qrcode_canvas = document.querySelector('#qrcode_canvas');
 
       if (this.qrcode_running) {
         this.qrcode_forcestop();
@@ -94,14 +217,15 @@ export default {
             requestAnimationFrame(this.qrcode_draw);
           return;
         }
-        this.qrcode_canvas.width = this.qrcode_video.videoWidth;
-        this.qrcode_canvas.height = this.qrcode_video.videoHeight;
-        this.qrcode_context = this.qrcode_canvas.getContext('2d');
+        var qrcode_canvas = document.querySelector('#qrcode_canvas');
+        qrcode_canvas.width = this.qrcode_video.videoWidth;
+        qrcode_canvas.height = this.qrcode_video.videoHeight;
+        qrcode_context = qrcode_canvas.getContext('2d');
       }
-      this.qrcode_context.drawImage(this.qrcode_video, 0, 0, this.qrcode_canvas.width, this.qrcode_canvas.height);
-      const imageData = this.qrcode_context.getImageData(0, 0, this.qrcode_canvas.width, this.qrcode_canvas.height);
+      this.qrcode_context.drawImage(this.qrcode_video, 0, 0, qrcode_canvas.width, qrcode_canvas.height);
+      const imageData = this.qrcode_context.getImageData(0, 0, qrcode_canvas.width, qrcode_canvas.height);
 
-      const code = jsQR(imageData.data, this.qrcode_canvas.width, this.qrcode_canvas.height);
+      const code = jsQR(imageData.data, qrcode_canvas.width, qrcode_canvas.height);
       if (code && code.data != "") {
         this.qrcode_scaned_data = code.data;
         console.log(code);
@@ -139,5 +263,7 @@ export default {
       this.qrcode_video.srcObject = null;
       this.qrcode_btn = 'QRスキャン開始';
     },
+  },
+  mounted: function(){
   }
 };
